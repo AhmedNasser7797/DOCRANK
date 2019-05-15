a6e330c8093760e28474a7d1fbdf15029989533d
Saif Allah Khaled <Saifallak@yahoo.com>
2019-05-15 07:27:17 +0200

    SignIn UI

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -1,210 +1,236 @@
 import 'package:final1/search_page.dart';
+import 'package:final1/signUp.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './signUp.dart';
 
 class SignIn extends StatefulWidget {
-
   @override
   _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
-  String _error_message="";
+  String _errorText = "";
+  final TextEditingController _userController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  final TextEditingController _userController=new TextEditingController();
-
-  final TextEditingController _passwordController=new TextEditingController();
-  final TextEditingController _emptyController=new TextEditingController();
-
-
-  void _empty(){
-    setState(() {
-      if(_userController.text.isEmpty || _passwordController.text.isEmpty)
-        _error_message="please enter you User Name and Password";
-        return ;
+  void _submit() {
+    if (_userController.text.length < 3 ||
+        _passwordController.text.length < 3) {
+      _errorText = "Please Enter Valid Email and Password";
+      if (mounted) setState(() {});
+    } else {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Search()));
     }
-    );
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title:
-        Text("Sign In") ,centerTitle: true,
-
+        title: Text("Sign In"),
+        centerTitle: true,
       ),
-
-      body: new Container(
-        alignment: Alignment.topCenter,
-
-        child: new Container(
-          padding: const EdgeInsets.only(
-              right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("assets/images/ss.jpg"),
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        padding:
+            EdgeInsets.only(right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/ss.jpg"),
+            fit: BoxFit.cover,
           ),
-
-
-         child: ListView(
-
-           children: <Widget>[
-
-             Container(
-               margin: const EdgeInsets.symmetric(vertical: 10.0),
-               height: 100.0,
-               width: 100.0,
-               decoration: new BoxDecoration(
-                 image: new DecorationImage(
-                   image: new AssetImage("assets/images/logo.png"),
-                   fit: BoxFit.contain,
-                 ),
-               ),
-             ),
-
-             Text(_error_message,style: TextStyle(color: Colors.red,fontSize: 20), ),
-
-             Container(
-                 height: 50,
-                 margin: const EdgeInsets.only(top:20.0,left: 10.0,right: 10.0, bottom: 30.0),
-                 child: new ListTile(
-                   leading: const Icon(Icons.person),
-                   title: new TextField(
-                     decoration: new InputDecoration(
-                       hintText: "User Name",
-                       border: InputBorder.none,
-                     ),
-                     controller: _userController,
-                   ),
-                 ),
-                 decoration: BoxDecoration(
-                     color: Colors.white,
-                     borderRadius: new BorderRadius.circular(30.0),
-                     border: Border.all(color: Colors.blue, width: 3))
-             ),
-
-             Container(
-                 height: 50,
-                 margin: const EdgeInsets.only(left: 10.0,right: 10.0,bottom: 30.0),
-                 child: new ListTile(
-                   leading: const Icon(Icons.phone_iphone),
-                   title: new TextField(
-                     decoration: new InputDecoration(
-                       hintText: "password",
-                       border: InputBorder.none,
-                     ),obscureText: true,
-
-                     controller: _passwordController,
-                   ),
-                 ),
-                 decoration: BoxDecoration(
-                     color: Colors.white,
-                     borderRadius: new BorderRadius.circular(30.0),
-                     border: Border.all(color: Colors.blue, width: 3)
-                 )
-             ),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: <Widget>[
-                 Container(
-                   alignment: Alignment.center,
-                   child: new InkWell(
-                     child: Text(
-                       "Have no account?  ",
-                       style: TextStyle(color: Colors.black),
-                     ),
-                     highlightColor: Colors.white,
-                     splashColor: Colors.white,
-                     onTap: () => Navigator.push(
-                       context,
-                       MaterialPageRoute(builder: (context) => SignUp()),
-                     ),
-                   ),
-                 ),
-
-                 Container(
-                   alignment: Alignment.center,
-                   child: new InkWell(
-                     child: Text(
-                       "Sign up here",
-                       style: TextStyle(color: Colors.blue),
-                     ),
-                     highlightColor: Colors.white,
-                     splashColor: Colors.white,
-                     onTap: () => Navigator.push(
-                       context,
-                       MaterialPageRoute(builder: (context) => SignUp()),
-                     ),
-                   ),
-                 ),
-
-               ],
-             ),
-
-
-
-              Padding(padding: EdgeInsets.only(top: 40)),
-
-              Row(
+        ),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              height: 100.0,
+              width: 100.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo.png"),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 40.0),
+              child: Text(
+                "Email",
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey.withOpacity(0.5),
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Row(
                 children: <Widget>[
-
-                  new RaisedButton(
-                    padding: const EdgeInsets.all(16.0),
-                    textColor: Colors.blue,
-                    color: Colors.white,
-                    onPressed: () => Navigator.pop(context),
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    child: new Text("Back"),
-                  ),
-
-                  Padding(padding: EdgeInsets.only(left: 160)),
-
-                  new RaisedButton(
-                    onPressed:/*_empty*/ () =>Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Search()),
-                    ),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    padding: const EdgeInsets.all(16.0),
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    child: new Text(
-                      "Sign In",
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.person_outline,
+                      color: Colors.grey,
                     ),
                   ),
-
-
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.grey.withOpacity(0.5),
+                    margin: EdgeInsets.only(left: 00.0, right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _userController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your email',
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  )
                 ],
-              )
-
-
-
-           ],
-         ),
-
-
-
-
-
+              ),
             ),
-
-
-
-
+            Padding(
+              padding: EdgeInsets.only(left: 40.0),
+              child: Text(
+                "Password",
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
             ),
-
-
-
-
-
-
-
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey.withOpacity(0.5),
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.grey.withOpacity(0.5),
+                    margin: EdgeInsets.only(left: 00.0, right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your password',
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Center(
+              child: Text(
+                "$_errorText",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      splashColor: Colors.blue,
+                      color: Colors.blue,
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: Text(
+                              "LOGIN",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          Transform.translate(
+                            offset: Offset(10.0, 0.0),
+                            child: Container(
+                              padding: EdgeInsets.all(5.0),
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(28.0)),
+                                splashColor: Colors.white,
+                                color: Colors.white,
+                                child: Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.blue,
+                                ),
+                                onPressed: () => _submit(),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      onPressed: () => _submit(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      color: Colors.transparent,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 20.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "DON'T HAVE AN ACCOUNT?",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => SignUp())),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,92 +1,89 @@
 import 'package:flutter/material.dart';
-import './sign_up.dart';
-import './question.dart';
-import './search.dart';
 
-class SignInPage extends StatefulWidget{
+import './search.dart';
+import './sign_up.dart';
+
+class SignInPage extends StatefulWidget {
   @override
   _SignInPageState createState() => _SignInPageState();
-
 }
 
-class _SignInPageState extends State<SignInPage>{
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-
-          title: new Center(
-            child:Text("Sign In") ,
-          )
-
-
+          centerTitle: true,
+          title: Text("Sign In"),
         ),
-        body: new Container(
-          padding: const EdgeInsets.only(
-              right: 20.0, left: 20.0, top: 20.0, bottom: 20.0),
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("assets/images/ss.jpg"),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/ss.jpg"),
               fit: BoxFit.cover,
             ),
           ),
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: Column(
-            // Column is also layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
-                margin: const EdgeInsets.symmetric(vertical: 40.0),
+                margin: EdgeInsets.symmetric(vertical: 40.0),
                 height: 100.0,
                 width: 100.0,
-                decoration: new BoxDecoration(
-                  image: new DecorationImage(
-                    image: new AssetImage("assets/images/logo.png"),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/logo.png"),
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
-              new Container(
-                  height: 50,
-                  margin: const EdgeInsets.only(top:50.0,left: 20.0,right: 20.0, bottom: 30.0),
-                  child: new ListTile(
-                    leading: const Icon(Icons.person),
-                    title: new TextField(
-                      decoration: new InputDecoration(
-                        hintText: "User Name",
-                        border: InputBorder.none,
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    new Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      child: Icon(
+                        Icons.person_outline,
+                        color: Colors.grey,
                       ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: new BorderRadius.circular(30.0),
-                      border: Border.all(color: Colors.blue, width: 3))),
-
-
-              new Container(
+                    Container(
+                      height: 30.0,
+                      width: 1.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      margin: const EdgeInsets.only(left: 00.0, right: 10.0),
+                    ),
+                    new Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your email',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
                   height: 50,
-                  margin: const EdgeInsets.only(left: 20.0,right: 20.0,bottom: 30.0),
-                  child: new ListTile(
-                    leading: const Icon(Icons.phone_iphone),
-                    title: new TextField(
-                      decoration: new InputDecoration(
+                  margin:
+                      EdgeInsets.only(left: 20.0, right: 20.0, bottom: 30.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone_iphone),
+                    title: TextField(
+                      decoration: InputDecoration(
                         hintText: "password",
                         border: InputBorder.none,
                       ),
@@ -94,11 +91,10 @@ class _SignInPageState extends State<SignInPage>{
                   ),
                   decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: new BorderRadius.circular(30.0),
+                      borderRadius: BorderRadius.circular(30.0),
                       border: Border.all(color: Colors.blue, width: 3))),
-
-              new Container(
-                child: new InkWell(
+              Container(
+                child: InkWell(
                   child: Text(
                     "Have no account? Sign up here",
                     style: TextStyle(color: Colors.blue),
@@ -106,46 +102,46 @@ class _SignInPageState extends State<SignInPage>{
                   highlightColor: Colors.white,
                   splashColor: Colors.white,
                   onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
-                  ),
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      ),
                 ),
               ),
-
-              new Expanded(
-                child: new Align(
+              Expanded(
+                child: Align(
                   alignment: FractionalOffset.bottomCenter,
-                  child: new Row(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      new Align(
+                      Align(
                         alignment: FractionalOffset.bottomLeft,
-                        child: new RaisedButton(
-                          padding: const EdgeInsets.all(16.0),
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(16.0),
                           textColor: Colors.blue,
                           color: Colors.white,
                           onPressed: () => Navigator.pop(context),
-                          shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0)),
-                          child: new Text("Back"),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Text("Back"),
                         ),
                       ),
                       Expanded(
-                        child: new Column(),
+                        child: Column(),
                       ),
-                      new Align(
+                      Align(
                         alignment: FractionalOffset.bottomRight,
-                        child: new RaisedButton(
-                          onPressed: () =>Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => search()),
-                          ),
+                        child: RaisedButton(
+                          onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => search()),
+                              ),
                           textColor: Colors.white,
                           color: Colors.blue,
-                          padding: const EdgeInsets.all(16.0),
-                          shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0)),
-                          child: new Text(
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Text(
                             "Done",
                           ),
                         ),
@@ -157,7 +153,6 @@ class _SignInPageState extends State<SignInPage>{
             ],
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
-    );
-
+        );
   }
 }
